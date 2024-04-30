e7ef431d0e694104089f1484b8c74fab0f28be32
Kshitish <kshitishsahu906@gmail.com>
2024-05-01 04:02:46 +0530

    added addtiotion function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+
+    static int additionOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
