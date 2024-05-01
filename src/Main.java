d75b09636fe4858223c5fab084a5883e48d3183c
kshitish sahu <kshitish.sahu@indusnet.co.in>
2024-05-01 16:46:09 +0530

    fiexed the issue

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
-    static int additionOfTwoNumbers(int a, int b) {
-        return a + b;
-    }
+
     static int subsOfTwoNumbers(int c, int d){
         return c-d;
     }
