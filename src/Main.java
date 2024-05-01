e0fc3fb7dd9e7c00b7a161778b108a9cee620124
kshitish sahu <kshitish.sahu@indusnet.co.in>
2024-05-01 16:14:51 +0530

    added substractuon function

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     static int additionOfTwoNumbers(int a, int b) {
         return a + b;
     }
+    static int substractonofTwoNumbers(int c, int d){
+        return c-d;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
